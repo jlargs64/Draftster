eb425c4d53ae4bc6ca34904830c4d37a04d0cbb2
Justin Largo <justinlargo7@gmail.com>
2020-06-12 16:07:46 -0400

    V1 of draftster is complete!

--- src/components/Draft/index.js
+++ src/components/Draft/index.js
@@ -9,54 +9,71 @@ import Row from 'react-bootstrap/Row';
 import './draft.css';
 
 export default function Draft(props) {
-	const room = props.room;
+  const room = props.room;
+  const selectPick = props.selectPick;
+  const currentTurn = room.currentTurn;
+  const isMyTurn = room.players[currentTurn] === room.name ? false : true;
+  function pickBtnClicked(pick) {
+    console.log(pick);
+    selectPick(pick);
+  }
 
-	return (
-		<div style={{ border: 'solid', padding: '1em' }}>
-			<h2>Draft</h2>
-			<hr />
-			<Row style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-				<h4>Current Round: {room.currentRound}</h4>
-				<h4>Current Turn: {room.currentTurn}</h4>
-			</Row>
+  return (
+    <div style={{ border: 'solid', padding: '1em' }}>
+      <h2>Draft</h2>
+      <hr />
+      {room.inProgress && (
+        <h3>It's {room.players[room.currentTurn]}'s turn to draft!</h3>
+      )}
 
-			<Row className='draft-order'>
-				<div className='draft-time-square'>
-					<p style={{ fontSize: '1.5em' }}>Time Left:</p>
-				</div>
-				{room.players.map((player, index) => (
-					<Card className='draft-square' key={index}>
-						<p style={{ fontSize: '1.5em', fontWeight: '500' }}>{player}</p>
-					</Card>
-				))}
-			</Row>
-			<hr />
-			<Table striped bordered hover responsive size='sm'>
-				<thead>
-					<tr>
-						<th style={{ width: '10px' }}>Rank</th>
-						<th>Symbol</th>
-						<th> </th>
-					</tr>
-				</thead>
-				<tbody>
-					{room.availablePicks.map((stock, index) => (
-						<tr key={index}>
-							<td>{index + 1}</td>
-							<td>{stock}</td>
-							<td>
-								<Button variant='success' block>
-									Pick
-								</Button>
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</Table>
-		</div>
-	);
+      <Row style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+        <h4>Current Round: {room.currentRound}</h4>
+        <h4>Current Turn: {room.currentTurn}</h4>
+      </Row>
+
+      <Row className="draft-order">
+        <div className="draft-time-square">
+          <p style={{ fontSize: '1.5em' }}>Time Left:</p>
+        </div>
+        {room.players.map((player, index) => (
+          <Card className="draft-square" key={index}>
+            <p style={{ fontSize: '1.5em', fontWeight: '500' }}>{player}</p>
+          </Card>
+        ))}
+      </Row>
+      <hr />
+      <Table striped bordered hover responsive size="sm">
+        <thead>
+          <tr>
+            <th style={{ width: '10px' }}>Rank</th>
+            <th>Symbol</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {room.availablePicks.map((pick, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{pick}</td>
+              <td>
+                <Button
+                  variant="success"
+                  onClick={() => pickBtnClicked(pick)}
+                  disabled={isMyTurn}
+                  block
+                >
+                  Pick
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 
 Draft.propTypes = {
-	room: PropTypes.object.isRequired,
+  room: PropTypes.object.isRequired,
+  selectPick: PropTypes.func.isRequired
 };
--- src/components/MyTeam/index.js
+++ src/components/MyTeam/index.js
@@ -1,41 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-//import Table from 'react-bootstrap/Table';
+import Table from 'react-bootstrap/Table';
 
 export default function MyTeam(props) {
-	//const room = props.room;
+  const room = props.room;
 
-	return (
-		<div
-			style={{
-				border: 'solid',
-				padding: '1em',
-				height: '100%',
-			}}
-		>
-			<h2>My Team</h2>
-			<hr />
-			{/* <Table striped bordered hover>
-				<thead>
-					<tr>
-						<th>Rank</th>
-						<th>Symbol</th>
-					</tr>
-				</thead>
-				<tbody>
-					{room.availablePicks.map((stock, index) => (
-						<tr key={index}>
-							<td>{index + 1}</td>
-							<td>{stock}</td>
-						</tr>
-					))}
-				</tbody>
-			</Table> */}
-		</div>
-	);
+  const playerPicks = room.playerPicks[room.name];
+  if (!playerPicks || playerPicks.length === 0) {
+    return (
+      <div
+        style={{
+          border: 'solid',
+          padding: '1em',
+          height: '100%'
+        }}
+      >
+        <h2>My Team</h2>
+        <hr />
+        <p>No picks yet! Start the draft to pick your team.</p>
+      </div>
+    );
+  } else {
+    return (
+      <div
+        style={{
+          border: 'solid',
+          padding: '1em',
+          height: '100%'
+        }}
+      >
+        <h2>My Team</h2>
+        <hr />
+        <Table striped bordered hover responsive size="sm">
+          <thead>
+            <tr>
+              <th>Pick</th>
+              <th>Symbol</th>
+            </tr>
+          </thead>
+          <tbody>
+            {room.playerPicks[room.name].map((pick, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{pick}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
 
 MyTeam.propTypes = {
-	room: PropTypes.object.isRequired,
+  room: PropTypes.object.isRequired
 };
