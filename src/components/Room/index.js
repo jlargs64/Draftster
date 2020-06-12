b23e184fd8e4801e5befd9b9ddd5c7d4152d024f
Justin Largo <justinlargo7@gmail.com>
2020-06-12 19:01:53 -0400

    Added active background color to current player turn tile.

--- src/components/Room/index.js
+++ src/components/Room/index.js
@@ -12,59 +12,66 @@ import Button from 'react-bootstrap/Button';
 import MyTeam from '../MyTeam';
 
 export default function Room(props) {
-  const room = props.room;
-  const sendMessage = props.sendMessage;
-  const lockRoom = props.lockRoom;
-  const selectPick = props.selectPick;
+	const room = props.room;
+	const sendMessage = props.sendMessage;
+	const lockRoom = props.lockRoom;
+	const selectPick = props.selectPick;
 
-  return (
-    <Container fluid style={{ padding: '1em' }}>
-      <Row>
-        <Col>
-          <h1>Room: {room.id}</h1>
-          <Row className="justify-content-center" style={{ padding: '1em' }}>
-            <Button
-              variant="primary"
-              onClick={lockRoom}
-              size="lg"
-              disabled={room.inProgress}
-            >
-              Start Draft
-            </Button>
-          </Row>
-        </Col>
-      </Row>
-      <Row style={{ display: 'flex', flexFlow: 'row wrap' }}>
-        <Col sm={8}>
-          <Draft room={room} selectPick={selectPick} />
-        </Col>
-        <Col
-          sm={4}
-          style={{
-            display: 'flex',
-            flexFlow: 'column nowrap',
-            justifyContent: 'space-evenly'
-          }}
-        >
-          <Row style={{ height: '50%' }}>
-            <Col>
-              <MyTeam room={room} />
-            </Col>
-          </Row>
-          <Row style={{ height: '50%' }}>
-            <Col>
-              <Chat room={room} sendMessage={sendMessage} />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
-  );
+	const currentTurn = room.currentTurn;
+
+	return (
+		<Container fluid style={{ padding: '1em' }}>
+			<Row>
+				<Col>
+					<h1>Room: {room.id}</h1>
+					<Row className='justify-content-center' style={{ padding: '1em' }}>
+						{!room.inProgress && (
+							<Button
+								variant='primary'
+								onClick={lockRoom}
+								size='lg'
+								disabled={room.inProgress}
+							>
+								Start Draft
+							</Button>
+						)}
+						{room.inProgress && (
+							<h3>It's {room.players[room.currentTurn]}'s turn to draft!</h3>
+						)}
+					</Row>
+				</Col>
+			</Row>
+			<Row style={{ display: 'flex', flexFlow: 'row wrap' }}>
+				<Col sm={8}>
+					<Draft room={room} selectPick={selectPick} />
+				</Col>
+				<Col
+					sm={4}
+					style={{
+						display: 'flex',
+						flexFlow: 'column nowrap',
+						justifyContent: 'space-evenly',
+					}}
+				>
+					<Row style={{ height: '50%' }}>
+						<Col>
+							<MyTeam room={room} />
+						</Col>
+					</Row>
+					<Row style={{ height: '50%' }}>
+						<Col>
+							<Chat room={room} sendMessage={sendMessage} />
+						</Col>
+					</Row>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
 
 Room.propTypes = {
-  room: PropTypes.object.isRequired,
-  sendMessage: PropTypes.func.isRequired,
-  lockRoom: PropTypes.func.isRequired,
-  selectPick: PropTypes.func.isRequired
+	room: PropTypes.object.isRequired,
+	sendMessage: PropTypes.func.isRequired,
+	lockRoom: PropTypes.func.isRequired,
+	selectPick: PropTypes.func.isRequired,
 };
